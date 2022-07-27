f5cc643d9995cf9c8f7aa678f1b634dee317d76b
Noam Cohen-Ofir <noamcohenofir@gmail.com>
2022-07-27 13:18:37 +0300

    finished forms, adds error message

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -6,18 +6,38 @@ app = Flask(  # Create a flask app
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
+app.config['SECRET_KEY'] = "icecream"
+
+username = "little_capy"
+password = "capybara809"
+facebook_friends=["pastor-greg","fred09","xx.emo_kidYx", "perryringlet", "rustymetal", "that_one_guy_in_all_of_my_lessons"]
 
 
-username = "llo2ay"
-password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
-
-
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'GET':
+		return render_template('login.html', wrong=False)
+	else:
+		form_username = request.form['username']
+		form_password = request.form['password']
+		print(form_username, form_password)
+		if (form_username == username and form_password == password):
+			return redirect(url_for('go_to_home'))
+		else:
+			return render_template('login.html', wrong=True)
 
+
+@app.route('/home')
+def go_to_home():
+	return render_template('home.html', facebook_friends=facebook_friends)
+
+@app.route('/friend_exists/<string:name>', methods=['GET', 'POST'])
+def go_to_friendexists(name):
+	if request.method == 'GET':
+		is_friend = "not"
+		if name in facebook_friends:
+			is_friend = ""
+		return render_template('/friend_exists.html', name=name, is_friend=is_friend)
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
